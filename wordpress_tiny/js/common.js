7a5fb00122be7180ec90f06b7790ccfd9d5f66a4
IgorAbdulla <54233851+IgorAbdulla@users.noreply.github.com>
2019-12-06 22:45:21 +0600

    Update common.js

--- wordpress_tiny/js/common.js
+++ wordpress_tiny/js/common.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
 
-// image
-var elements = $('.right_side_post img');
 
-for (var i = 0; i < elements.length; i++) {
-	let image = new Image();
-	image.src = elements[i].src;
-	if (image.height < 270) {
-		$(elements[i]).css('height', '100%')
-		$(elements[i]).css('position', 'static')
-	}
-}
 
 // hamburger
 (function () {
