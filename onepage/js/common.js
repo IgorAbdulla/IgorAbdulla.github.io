1dd1e058bd269bd14534084bc0aaf711680acdb7
IgorAbdulla <54233851+IgorAbdulla@users.noreply.github.com>
2020-05-02 17:06:58 +0600

    6

--- onepage/js/common.js
+++ onepage/js/common.js
@@ -1,21 +1,22 @@
-"use strict";
+// "use strict";
 
 window.addEventListener("load", function () {
 
 
-
 // faq
 
-	let question = document.querySelector(".questions");
+let question = document.querySelector(".questions");
 
-	question.addEventListener("click", function(e) {
-		e.preventDefault();
-		if(!e.target.closest(".show-answer")) return
+question.addEventListener("click", function(e) {
+	e.preventDefault();
+	if(!e.target.closest(".show-answer")) return
 
-			let parent = e.target.closest(".questions__item");
-		let answer = parent.querySelector(".questions__item-content");
-		answer.classList.toggle("_visable")
-	})
+		let parent = e.target.closest(".questions__item");
+	let answer = parent.querySelector(".questions__item-content");
+	answer.classList.toggle("_visable")
+})
+
 
 
 });
+
